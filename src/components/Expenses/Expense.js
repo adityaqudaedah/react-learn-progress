cc1b8af769d1e6cde7c7d8d48f0ba2918a7d447b
Rahmat Aditya Qudaedah <adityaqudaedah@gmail.com>
2021-10-08 17:16:11 +0700

    finishing rendering list module

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -4,13 +4,36 @@ import ExpenseItems from "./ExpenseItems";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseFilter from "./ExpensesFilter";
+import not_found from "../../img/not_found.svg";
 
 function Expense(props) {
   const [filteredYear, setFilteredYear] = useState("2021");
   const changeFilterHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log(`log from Expense.js selected year :\n ${selectedYear}`);
+
+    // console.log(`log from Expense.js selected year :\n ${selectedYear}`);
   };
+  const filteredExpenses = props.data.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expenseContent = (
+    <div className="expenses_not_found">
+      <img src={not_found} alt="" /> <p>Expenses not found here...</p>
+    </div>
+  );
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItems
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -18,15 +41,7 @@ function Expense(props) {
           selected={filteredYear}
           onChangeFilter={changeFilterHandler}
         />
-
-        {props.data.map(expense => 
-          <ExpenseItems
-            key = {expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        )}
+        {expenseContent}
       </Card>
     </div>
   );
